8ee86b34ba488233f906eacc3138fa5ff76efe30
nakaminekatsuo <nakamine.katsuo@gmail.com>
2025-03-23 12:35:10 +0900

    env: add vite-plugin-checker.

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import babel from "vite-plugin-babel";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { checker } from "vite-plugin-checker";
 
 export default defineConfig({
-  plugins: [babel({ filter: /\.tsx?$/ }), reactRouter(), tsconfigPaths()],
+  plugins: [
+    checker({}),
+    babel({ filter: /\.tsx?$/ }),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
 });
